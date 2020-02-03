3b87833137b31be77b07e5ceef1b4c6d235e589f
MikhailovSergey <mikhailovss@plavalaguna.ru>
2020-02-03 03:58:27 +0300

    - update routine perfomance

--- unigame.routines/Assets/UniGame.Routines/Examples/UpdateExamples/UpdateRoutineExample.cs
+++ unigame.routines/Assets/UniGame.Routines/Examples/UpdateExamples/UpdateRoutineExample.cs
@@ -3,26 +3,37 @@ using UnityEngine;
 
 namespace UniGreenModules.UniRoutine.Examples.UpdateExamples
 {
+    using System;
+    using System.Collections.Generic;
     using Runtime;
+    using Runtime.Extension;
 
     public class UpdateRoutineExample : MonoBehaviour
     {
         public int         updateCount;
         public int         routineCount;
         public RoutineType RoutineType = RoutineType.Update;
-    
+        public List<RoutineHandler> RoutineHandlers = new List<RoutineHandler>();
+            
         // Start is called before the first frame update
         private void Start()
         {
             for (int i = 0; i < routineCount; i++) {
-                OnUpdate(i).ExecuteRoutine(RoutineType, false);
+                var handler = OnUpdate(i).Execute(RoutineType, false);
+                RoutineHandlers.Add(handler);
             }
+        }
+
+        private void OnDisable()
+        {
+            RoutineHandlers.ForEach(x => x.Cancel());
+            RoutineHandlers.Clear();
         }
 
         private IEnumerator OnUpdate(int number)
         {
             var counter = 0;
-            while (this && isActiveAndEnabled) {
+            while (true) {
                 counter++;
                 if (counter > updateCount) {
                     Debug.Log($"ROUTINE: COUNTER #{number} {RoutineType} FINISHED");

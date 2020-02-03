136d8d57a3bcbf5b9128d0498b0af75b8daf1f8a
MikhailovSergey <mikhailovss@plavalaguna.ru>
2020-02-03 04:08:28 +0300

    - update routine perfomance

--- unigame.routines/Assets/UniGame.Routines/Examples/UpdateExamples/MicroRoutineUpdater.cs
+++ unigame.routines/Assets/UniGame.Routines/Examples/UpdateExamples/MicroRoutineUpdater.cs
@@ -3,24 +3,33 @@ using UnityEngine;
 
 namespace UniGreenModules.UniRoutine.Examples.UpdateExamples
 {
+    using System;
+    using System.Collections.Generic;
     using UniRx;
 
     public class MicroRoutineUpdater : MonoBehaviour
     {
         public int updateCount;
         public int routineCount;
+        public List<IDisposable> Disposables = new List<IDisposable>();
         // Start is called before the first frame update
         private void Start()
         {
             for (int i = 0; i < routineCount; i++) {
-                Observable.FromMicroCoroutine(() => OnUpdate(i)).Subscribe();
+                Observable.FromMicroCoroutine(() => OnUpdate(i)).Subscribe().AddTo(Disposables);
             }
+        }
+
+        private void OnDisable()
+        {
+            Disposables.ForEach(x => x.Dispose());
+            Disposables.Clear();
         }
 
         private IEnumerator OnUpdate(int number)
         {
             var counter = 0;
-            while (this && isActiveAndEnabled) {
+            while (true) {
                 counter++;
                 if (counter > updateCount) {
                     Debug.Log($"UniRx MicroRoutine: COUNTER #{number}  FINISHED");

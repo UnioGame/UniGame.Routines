021af0d0215f127be032cc395c453fbfbbe3f5f0
MikhailovSergei <mikhailovs@taktikagames.ru>
2020-02-25 17:53:41 +0300

    - add routine chain example

---
+++ unigame.routines/Assets/UniGame.Routines/Examples/RoutineChain/RoutineChainTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniGreenModules.UniRoutine.Runtime;
+using UniGreenModules.UniRoutine.Runtime.Extension;
+using UnityEngine;
+
+public class RoutineChainTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        LongOp1().Execute();
+    }
+
+    private IEnumerator LongOp1()
+    {
+        var counter = 0;
+        while (counter < 100) {
+            yield return LongOp2();
+            yield return LongOp3();
+        }
+    }
+    
+    private IEnumerator LongOp2()
+    {
+        yield return null;
+    }
+    
+    private IEnumerator LongOp3()
+    {
+        yield return this.WaitForSecond(0.1f);
+    }
+    
+}

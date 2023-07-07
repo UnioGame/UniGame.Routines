3cdfe082407a85173710c89cba45ecd3c67f8574
Mefodei <mefodei.link@gmail.com>
2023-07-08 00:07:31 +0700

    - update animation extension

--- unigame.routines/Runtime/Extension/AnimatorExtensions.cs
+++ unigame.routines/Runtime/Extension/AnimatorExtensions.cs
@@ -8,13 +8,17 @@
 
     public static class AnimatorExtensions
     {
-        public static IEnumerator WaitForEnd(this Animator animator, int stateHash, int layer = 0)
+        
+        public static async UniTask WaitStateEndAsync(this Animator animator, int stateHash, int layer = 0)
         {
-            while (animator == null || animator.GetCurrentAnimatorStateInfo(layer).shortNameHash != stateHash) {
-                yield return null;
+            if (animator == null || !animator.HasState(layer, stateHash)) {
+                var nextState = animator.GetNextAnimatorStateInfo(layer);
+                return;
             }
+            
+            animator.SetTrigger(stateHash);
 
-            yield return animator.WaitForSeconds(animator.GetCurrentAnimatorStateInfo(layer).length);
+            await animator.WaitForEndAsync(stateHash, layer);
         }
 
         public static async UniTask WaitForEndAsync(this Animator animator, int stateHash, int layer = 0)
@@ -23,6 +27,15 @@
                 await UniTask.Yield();
 
             await UniTask.Delay(TimeSpan.FromSeconds(animator.GetCurrentAnimatorStateInfo(layer).length));
+        }
+        
+        public static IEnumerator WaitForEnd(this Animator animator, int stateHash, int layer = 0)
+        {
+            while (animator == null || animator.GetCurrentAnimatorStateInfo(layer).shortNameHash != stateHash) {
+                yield return null;
+            }
+
+            yield return animator.WaitForSeconds(animator.GetCurrentAnimatorStateInfo(layer).length);
         }
         
         public static IEnumerator WaitStateEnd(this Animator animator, int stateHash, int layer = 0)
@@ -37,17 +50,7 @@
             yield return animator.WaitForEnd(stateHash, layer);
         }
         
-        public static async UniTask WaitStateEndAsync(this Animator animator, int stateHash, int layer = 0)
-        {
-            if (animator == null || !animator.HasState(layer, stateHash)) {
-                var nextState = animator.GetNextAnimatorStateInfo(layer);
-                return;
-            }
-            
-            animator.SetTrigger(stateHash);
-
-            await animator.WaitForEndAsync(stateHash, layer);
-        }
+        
         
         
     }

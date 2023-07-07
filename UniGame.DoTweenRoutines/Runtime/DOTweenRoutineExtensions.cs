8af10ceb5fd5937e41dda808859775c97d292198
Mefodei <mefodei.link@gmail.com>
2023-07-07 22:18:58 +0700

    - update routines extensions

--- UniGame.DoTweenRoutines/Runtime/DOTweenRoutineExtensions.cs
+++ UniGame.DoTweenRoutines/Runtime/DOTweenRoutineExtensions.cs
@@ -1,31 +1,79 @@
 ﻿namespace UniModules.UniGame.DoTweenRoutines.Runtime
 {
     using System.Collections;
+    using Cysharp.Threading.Tasks;
     using DG.Tweening;
 
     public static class DOTweenRoutineExtensions
     {
+        
+        public static async UniTask<Tween> WaitForCompletionTweenAsync(this Tween t)
+        {
+            while (t.active && !t.IsComplete())
+                await UniTask.Yield(PlayerLoopTiming.PostLateUpdate);
+            return t;
+        }
+
+        public static async UniTask<Tween> WaitForRewindTweenAsync(this Tween t)
+        {
+            while (t.active && (!t.playedOnce || t.position * (t.CompletedLoops() + 1) > 0.0)) {
+                await UniTask.Yield(PlayerLoopTiming.PostLateUpdate);
+            }
+            return t;
+        }
+
+        public static async UniTask<Tween> WaitForKillTweenAsync(this Tween t)
+        {
+            while (t.active) {
+                await UniTask.Yield(PlayerLoopTiming.PostLateUpdate);
+            }
+            return t;
+        }
+        public static async UniTask<Tween> WaitForElapsedLoopTweenAsync(this Tween t, int elapsedLoops)
+        {
+            while (t.active && t.CompletedLoops() < elapsedLoops) {
+                await UniTask.Yield(PlayerLoopTiming.PostLateUpdate);
+            }
+            return t;
+        }
+
+        public static async UniTask<Tween> WaitForPositionTweenAsync(this Tween t, float position)
+        {
+            while (t.active && t.position * (t.CompletedLoops() + 1) < position) {
+                await UniTask.Yield(PlayerLoopTiming.PostLateUpdate);
+            }
+            return t;
+        }
+        
+        public static async UniTask<Tween> WaitForStartTweenAsync(this Tween t)
+        {
+            while (t.active && !t.playedOnce) {
+                await UniTask.Yield(PlayerLoopTiming.PostLateUpdate);
+            }
+            return t;
+        }
+
         public static IEnumerator WaitForCompletionTween(this Tween t)
         {
             while (t.active && !t.IsComplete()) {
                 yield return null;
             }
         }
-
+        
         public static IEnumerator WaitForRewindTween(this Tween t)
         {
             while (t.active && (!t.playedOnce || t.position * (t.CompletedLoops() + 1) > 0.0)) {
                 yield return null;
             }
         }
-
+        
         public static IEnumerator WaitForKillTween(this Tween t)
         {
             while (t.active) {
                 yield return null;
             }
         }
-
+        
         public static IEnumerator WaitForElapsedLoopTween(this Tween t, int elapsedLoops)
         {
             while (t.active && t.CompletedLoops() < elapsedLoops) {
@@ -39,7 +87,7 @@
                 yield return null;
             }
         }
-
+        
         public static IEnumerator WaitForStartTween(this Tween t)
         {
             while (t.active && !t.playedOnce) {
--- unigame.routines/Runtime/Extension/AnimatorExtensions.cs
+++ unigame.routines/Runtime/Extension/AnimatorExtensions.cs
@@ -1,6 +1,8 @@
 ﻿namespace UniGame.Routines.Runtime.Extension
 {
+    using System;
     using System.Collections;
+    using Cysharp.Threading.Tasks;
     using UniModules.UniRoutine.Runtime.Extension;
     using UnityEngine;
 
@@ -15,6 +17,14 @@
             yield return animator.WaitForSeconds(animator.GetCurrentAnimatorStateInfo(layer).length);
         }
 
+        public static async UniTask WaitForEndAsync(this Animator animator, int stateHash, int layer = 0)
+        {
+            while (animator == null || animator.GetCurrentAnimatorStateInfo(layer).shortNameHash != stateHash)
+                await UniTask.Yield();
+
+            await UniTask.Delay(TimeSpan.FromSeconds(animator.GetCurrentAnimatorStateInfo(layer).length));
+        }
+        
         public static IEnumerator WaitStateEnd(this Animator animator, int stateHash, int layer = 0)
         {
             if (animator == null || !animator.HasState(layer, stateHash)) {
@@ -26,5 +36,19 @@
 
             yield return animator.WaitForEnd(stateHash, layer);
         }
+        
+        public static async UniTask WaitStateEndAsync(this Animator animator, int stateHash, int layer = 0)
+        {
+            if (animator == null || !animator.HasState(layer, stateHash)) {
+                var nextState = animator.GetNextAnimatorStateInfo(layer);
+                return;
+            }
+            
+            animator.SetTrigger(stateHash);
+
+            await animator.WaitForEndAsync(stateHash, layer);
+        }
+        
+        
     }
 }

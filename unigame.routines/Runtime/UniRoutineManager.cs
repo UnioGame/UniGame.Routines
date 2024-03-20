1c18f5b5a4514fa830061a58d5e72abdb1974116
mefodei <mefodei.link@gmail.com>
2024-03-20 22:45:50 +0700

    - fix namespace

--- unigame.routines/Runtime/UniRoutineManager.cs
+++ unigame.routines/Runtime/UniRoutineManager.cs
@@ -8,6 +8,8 @@ namespace UniModules.UniRoutine.Runtime
     using UnityEngine;
 
 #if ENABLE_IL2CPP
+    using Unity.IL2CPP.CompilerServices;
+
     [Il2CppSetOption(Option.NullChecks, false)]
     [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
     [Il2CppSetOption(Option.DivideByZeroChecks, false)]
--- unigame.routines/Runtime/UniRoutineTask.cs
+++ unigame.routines/Runtime/UniRoutineTask.cs
@@ -10,6 +10,8 @@
     using Unity.IL2CPP.CompilerServices;
 
 #if ENABLE_IL2CPP
+    using Unity.IL2CPP.CompilerServices;
+
     [Il2CppSetOption(Option.NullChecks, false)]
     [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
     [Il2CppSetOption(Option.DivideByZeroChecks, false)]
